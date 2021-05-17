5ee077c075b35e82838c2cae8fe1a329ddc15547
Shivansh Raghav <shivansh901@gmail.com>
2021-05-17 23:31:07 +0530

    other login callback Url fix

--- config/services.php
+++ config/services.php
@@ -32,17 +32,17 @@ return [
     'github' => [
         'client_id' => '24fbd0edd46019058da7', //Github API
         'client_secret' => '4500e6ab7a9bb70608600fbdee37969276bb0685', //Github Secret
-        'redirect' => 'http://localhost:8000/login/github/callback',
+        'redirect' => 'https://www.grest.in/login/github/callback',
      ],
      'google' => [
         'client_id' => '122886334250-fsfd8ugugkr0hjrkakukbgnnruomjn95.apps.googleusercontent.com', //Google API
         'client_secret' => 'eyuwbzXGBgbebzPXNiqSOods', //Google Secret
-        'redirect' => 'http://localhost:8000/login/google/callback',
+        'redirect' => 'https://www.grest.in/login/google/callback',
      ],
      'facebook' => [
         'client_id' => '1494792524037846', //Facebook API
         'client_secret' => '1bf13c2faf680f4ae2b97bdafa92c7d2', //Facebook Secret
-        'redirect' => 'http://localhost:8000/login/facebook/callback',
+        'redirect' => 'https://www.grest.in/login/facebook/callback',
      ],
 
 ];
